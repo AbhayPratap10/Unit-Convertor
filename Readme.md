4d0ee5143b2af00026141ef62c75424bdce3ee98
Abhay Pratap Singh <abhaypratap3062002@gmail.com>
2023-09-20 14:17:37 +0530

    update the files
    added a readme
    make the code more readable

---
+++ Readme.md
@@ -0,0 +1,41 @@
+
+# Unit Converter
+
+Unit Converter is a simple web application that allows users to convert between metric and imperial units for length, volume, and mass. This project serves as a practical tool for quick unit conversions. It is built using HTML, CSS, and JavaScript.
+
+## Features
+
+- Convert between:
+  - Length (Meters and Feet)
+  - Volume (Liters and Gallons)
+  - Mass (Kilograms and Pounds)
+
+## Getting Started
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/AbhayPratap10/unit-converter.git
+   ```
+
+2. Open `index.html` in your web browser to run the application.
+
+## Usage
+
+1. Enter a value in the input field.
+2. Click the "Convert" button to see the converted values for length, volume, and mass.
+
+## Screenshots
+
+![Unit Converter Screenshot](Screenshot.png)
+
+## Contributing
+
+Contributions are welcome! If you'd like to improve this project, please follow these steps:
+
+1. Fork the project.
+2. Create your feature branch (`git checkout -b feature/your-feature-name`).
+3. Commit your changes (`git commit -m 'Add some feature'`).
+4. Push to the branch (`git push origin feature/your-feature-name`).
+5. Open a pull request.
+
--- index.js
+++ index.js
@@ -1,25 +1,44 @@
+
+//get reference to DOM manipulation
 const btnEl=document.getElementById("btn-el")
-const lengthEl=document.getElementById("length-el")
 const inputEl=document.getElementById("input-el")
+const lengthEl=document.getElementById("length-el")
 const volumeEl=document.getElementById("volume-el")
 const massEl=document.getElementById("mass-el")
 
+//Conversion functions 
+function convertLength(value){
+    const meterToFeet=value * 3.281
+    const feetToMeter=value / 3.281
+    return `${value} meters = ${meterToFeet.toFixed(3)} feet | ${value} feet = ${feetToMeter.toFixed(3)} meters`
+}
+
+function convertVolume(value){
+    const literToGallon=value * 0.264
+    const gallonToLiter=value / 0.264
+    return `${value} liters = ${literToGallon.toFixed(3)} gallon | ${value} gallon = ${gallonToLiter.toFixed(3)} liters`
+}
+
+function convertMass(value){
+    const kgToPound=value * 2.204
+    const poundToKg=value / 2.204
+
+    return `${value} Kg = ${kgToPound.toFixed(3)} pound | ${value} pound = ${poundToKg.toFixed(3)} kg`
+}
+
+//convert butoon listener
+
 btnEl.addEventListener("click",function(){
-    let inputValue=inputEl.value
+    const inputValue = parseFloat(inputEl.value)
 
-    lengthEl.textContent=""
-    volumeEl.textContent=""
-    massEl.textContent=""
-
-    let meterToFeet=inputValue * 3.281
-    let feetToMeter=inputValue / 3.281
-    let literToGallon=inputValue * 0.264
-    let gallonToLiter=inputValue / 0.264
-    let kgToPound=inputValue * 2.204
-    let poundToKg=inputValue / 2.204
-
-    lengthEl.innerHTML+=`${inputValue} meters = ${meterToFeet.toFixed(3)} feet | ${inputValue} feet = ${feetToMeter.toFixed(3)} meters`
-    volumeEl.innerHTML+=`${inputValue} liters = ${literToGallon.toFixed(3)} gallon | ${inputValue} gallon = ${gallonToLiter.toFixed(3)} liters`
-    massEl.innerHTML+=`${inputValue} Kg = ${kgToPound.toFixed(3)} pound | ${inputValue} pound = ${poundToKg.toFixed(3)} kg`
+    if(!isNaN(inputValue)){
+    lengthEl.textContent = convertLength(inputValue)
+    volumeEl.textContent = convertVolume(inputValue)
+    massEl.textContent = convertMass(inputValue)
+    }else{
+        lengthEl.textContent = "Invalid input"
+        volumeEl.textContent = "Invalid input"
+        massEl.textContent = "Invalid input"
+    }
 })
 
